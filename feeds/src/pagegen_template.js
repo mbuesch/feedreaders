93a5bb18edd47556873b8cab1e5c7f05967ae788
Michael Buesch <m@bues.ch>
2025-01-06 14:42:57 +0100

    js: Null the request object after use

--- feeds/src/pagegen_template.js
+++ feeds/src/pagegen_template.js
@@ -14,26 +14,30 @@ function send_feed_update_rev_request() {
             var elem_rev_dynamic = document.getElementById("feed_update_revision_dynamic");
             var elem_feed_list_th = document.getElementById("feed_list_th");
             var elem_feed_list_th_a = document.getElementById("feed_list_th_a");
+            var again = false;
 
             if (elem_rev_static && elem_rev_dynamic && elem_feed_list_th && elem_feed_list_th_a) {
-                var again = true;
-
                 if (feed_update_rev_request.status == 200) { // Ok
                     elem_rev_dynamic.textContent = feed_update_rev_request.responseText;
                     var feed_update_rev_dynamic = parseInt(elem_rev_dynamic.textContent);
                     var feed_update_rev_static = parseInt(elem_rev_static.textContent);
 
-                    if (feed_update_rev_dynamic != feed_update_rev_static) {
+                    if (feed_update_rev_dynamic == feed_update_rev_static) {
+                        again = true;
+                    } else {
                         elem_feed_list_th_a.textContent += " (UPDATES AVAILABLE)";
                         elem_feed_list_th.style.color = "red";
                         elem_feed_list_th.style.fontWeight = "bold";
-                        again = false;
                     }
+                } else {
+                    again = true;
                 }
+            }
 
-                if (again) {
-                    setTimeout(send_feed_update_rev_request, 10000);
-                }
+            feed_update_rev_request = null;
+
+            if (again) {
+                setTimeout(send_feed_update_rev_request, 10000);
             }
         }
     };
